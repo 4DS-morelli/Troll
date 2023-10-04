ff40638449ae51b3c424a0e24a9bc1e63e6d4b21
nicola.morelli <nicola.morelli@buonarroti.tn.it>
2023-10-04 18:17:19 +0200

    feat: created constructor and getter and setter methods

--- src/Troll.java
+++ src/Troll.java
@@ -4,5 +4,42 @@ public class Troll {
     private int altezza;
     private int eta;
 
+    public Troll(int vita, String nome, int altezza, int eta) {
+        setVita(vita);
+        setNome(nome);
+        setAltezza(altezza);
+        setEta(eta);
+    }
 
+    public void setVita(int vita) {
+        this.vita = vita;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAltezza(int altezza) {
+        this.altezza = altezza;
+    }
+
+    public void setEta(int eta) {
+        this.eta = eta;
+    }
+
+    public int getVita() {
+        return vita;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAltezza() {
+        return altezza;
+    }
+
+    public int getEta() {
+        return eta;
+    }
 }
