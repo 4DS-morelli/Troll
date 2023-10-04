5a529e5672132c43399c48079425dc65869fe598
nicola.morelli <nicola.morelli@buonarroti.tn.it>
2023-10-04 18:20:51 +0200

    feat: added variables vita, eta, altezza, nome in the main

--- src/testTroll.java
+++ src/testTroll.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class testTroll {
+    public static void main(String[] args) {
+
+        Scanner input= new Scanner(System.in);
+
+        int vita, eta, altezza;
+        String nome;
+
+    }
 }
