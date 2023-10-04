3b0a8ae0573a224e538377122eb6fad245424dab
nicola.morelli <nicola.morelli@buonarroti.tn.it>
2023-10-04 18:25:34 +0200

    test: modified inputs in the main

--- src/testTroll.java
+++ src/testTroll.java
@@ -13,13 +13,13 @@ public class testTroll {
         System.out.print("Inserisci il nome del troll: ");
         nome= input.next();
 
-        System.out.print("Inserisci la vita del troll: ");
+        System.out.print("Inserisci la vita del troll (in punti vita): ");
         vita= input.nextInt();
 
         System.out.print("Inserisci l'età del troll: ");
         eta= input.nextInt();
 
-        System.out.print("Inserisci l'altezza del troll: ");
+        System.out.print("Inserisci l'altezza del troll (in metri): ");
         altezza= input.nextInt();
 
         t1= new Troll(vita, nome, altezza, eta);
