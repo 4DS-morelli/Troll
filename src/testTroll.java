0e4d0eb6192179dacaece015644eb802956ae2a1
nicola.morelli <nicola.morelli@buonarroti.tn.it>
2023-10-04 18:24:09 +0200

    test: added input for all the variables in the main

--- src/testTroll.java
+++ src/testTroll.java
@@ -8,5 +8,21 @@ public class testTroll {
         int vita, eta, altezza;
         String nome;
 
+        Troll t1;
+
+        System.out.print("Inserisci il nome del troll: ");
+        nome= input.next();
+
+        System.out.print("Inserisci la vita del troll: ");
+        vita= input.nextInt();
+
+        System.out.print("Inserisci l'età del troll: ");
+        eta= input.nextInt();
+
+        System.out.print("Inserisci l'altezza del troll: ");
+        altezza= input.nextInt();
+
+        t1= new Troll(vita, nome, altezza, eta);
+
     }
 }
